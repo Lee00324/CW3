34f33d9a1878ab642764582c1d42e604c7dae8a1
Lee00324 <151765177+Lee00324@users.noreply.github.com>
2024-05-17 19:30:05 +0800

    Add files via upload

---
+++ release/survis_0.1.0_20151022/src/data/generated/bib.js
@@ -0,0 +1,18 @@
+﻿define({ entries : {
+    "The application of artificial intelligence in board games": {
+        "abstract": "The development of artificial intelligence in games has been going on for a long time. In the case of board games, artificial intelligence helps people to play against computers alone. While there have been instances where computers have beaten real people, artificial intelligence is still being refined and modified. In this work, the aim is to investigate how algorithms are used to create artificial intelligence in four different board games. For existing tic-tac-toe, gobang, go game, and chess research, different algorithms or systems are applied to different chess games. From simple to difficult, researchers have come up with more algorithms to screen out the most suitable ones to be applied. For the simpler tic-tac-toe, the researchers also adopted a simpler search tree. As the board size improves in gobang game, the researchers increase more algorithm tools in ordinary search trees to save time. In the later more complex go and chess, the researchers compared some existing techniques and analysed the advantages and disadvantages of various algorithms, and finally concluded the best application for a particular algorithm. Even though current algorithms can help AI run fast and compete with real people, it is believed that more efficient algorithms will emerge in the future to further enhance the power of AI.",
+        "author": "Jiang, Chengyi",
+        "doi": "https://doi.org/10.54254/2755-2721/4/20230497",
+        "journal": "Applied and Computational Engineering",
+        "keywords": "Artificial intelligence, Board game, Algorithms",
+        "number": "01",
+        "pages": "383\u2013386",
+        "publisher": "Applied and Computational Engineering",
+        "series": "TVCG",
+        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+        "type": "article",
+        "url": "https://ace.ewapublishing.org/article/fd3ae72ae5824c67829e5baa31dc4ffc",
+        "volume": "4",
+        "year": "2023"
+    }
+}});
